4c766959b5da1e327694c5466f04351f85c57e66
JoseDRamirezM <55093568+JoseDRamirezM@users.noreply.github.com>
2019-10-10 15:30:46 -0500

    Add files via upload
    Varios Sprites en el Frame

---
+++ PersonajeAbstractFactory/src/Control/Teclado.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Control;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Teclado implements KeyListener {
+    
+    private final static int numeroTeclas = 120;
+    private final boolean[] teclas = new boolean[numeroTeclas];
+    
+    public boolean arriba;
+    public boolean abajo;
+    public boolean izquierda;
+    public boolean derecha;
+    public boolean atacar;
+    
+    public void actualizar(){
+        arriba = teclas[KeyEvent.VK_W];
+        abajo = teclas[KeyEvent.VK_S];
+        derecha = teclas[KeyEvent.VK_D];
+        izquierda = teclas[KeyEvent.VK_A];
+        atacar = teclas[KeyEvent.VK_X];
+    }
+    
+    @Override
+    public void keyTyped(KeyEvent e) {    
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        teclas[e.getKeyCode()] = true;
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        teclas[e.getKeyCode()] = false;
+    }
+    
+}
